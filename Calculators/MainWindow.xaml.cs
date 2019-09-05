2665fbd4a481a424d7bac76389dc2c67dae255bd
Adeboye Azeez <AAzeez@spartaglobal.com>
2019-09-05 09:29:16 +0100

    WPF

---
+++ Calculators/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculators
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            int txt1 = int.Parse(textbox1.Text);
+            int txt2 = int.Parse(textbox2.Text);
+               int   answer = 0;
+
+            if (ComboBox1.Text == "+")
+                answer = (txt1 + txt2);
+            else if (ComboBox1.Text == "-")
+                answer = (txt1 - txt2);
+            else if (ComboBox1.Text == "*")
+                answer = (txt1 * txt2);
+            else if (ComboBox1.Text == "/")
+                answer = (txt1 / txt2);
+
+            MessageBox.Show(answer.ToString());
+        }
+    }
+}
---
+++ Calculte/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Calculate
+        {
+            public double Add(double num1, double num2)
+            {
+                return num1 + num2;
+            }
+
+            public double Sub(double num1, double num2)
+            {
+                return num1 - num2;
+            }
+
+            public double Div(double num1, double num2)
+            {
+                return num1 / num2;
+            }
+
+            public double Mult(double num1, double num2)
+            {
+                return num1 * num2;
+            }
+        }
+    }
+}
